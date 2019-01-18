fe0765e692531b2c76f2fbbce625cd37cd45d5f8
Selinatesfa <45018588+Selinatesfa@users.noreply.github.com>
2019-01-18 16:39:31 -0600

    Update exercise2child.js

--- homework04/exercise2child.js
+++ homework04/exercise2child.js
@@ -2,10 +2,7 @@
 const fs= require('fs');
 const url= require('url');
 
-const myurl=url.parse("http://localhost:3000/?url=test.html",true);
-
-
-   
+ 
     process.on('message',(msg)=>{
 
     var html=fs.readFileSync(msg,'utf8');

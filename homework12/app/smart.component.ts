48dedebf9130310ff911c380b6a266f60257de6b
Selinatesfa <45018588+Selinatesfa@users.noreply.github.com>
2019-01-31 18:33:59 -0600

    Update smart.component.ts

--- homework12/app/smart.component.ts
+++ homework12/app/smart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggablePipe } from './loggable.pipe';
 
 @Component({
   selector: 'app-smart',
@@ -6,17 +7,25 @@ import { Component, OnInit } from '@angular/core';
     <p>
         <app-dumb [items]="sitems"></app-dumb>
       </p>
-      <input #val (keyup)='0' />
-<p> {{val.value | loggable}}</p>
-<p isvisible [result]='true' >hello</p>
+      <input #val (click)='0'  />
+      <p #dval (click)='0'>hhhh</p>
+      <p  >{{dval.innerHTML|loggable}} </p>
+      <p >{{val.value|loggable}}</p>
+   
+<p isvisible [result]=true >paragraph is visible or not</p>
   `,
   styles: []
 })
 export class SmartComponent implements OnInit {
 public sitems=[3,4,5,6,7];
   constructor() { }
-  
+
+
   ngOnInit() {
   }
 
+  onClick(dval) {
+    
+    console.log(dval);
+  }
 }
